93b3a3c22122ce20ec0e67360c233ced6fda41ca
Kyle Zhang <kyle.z0929@gmail.com>
2021-06-13 13:50:48 -0400

    testing some stuff

--- index.js
+++ index.js
@@ -11,12 +11,13 @@ function getAuth(client_id, redirect_uri, scope_entities){
 
 async function get_tokens(twitch_client, twitch_secret, code, redirect, body){
     const raw = await axios.post(`https://id.twitch.tv/oauth2/token?client_id=${twitch_client}&client_secret=${twitch_secret}&code=${code}&grant_type=authorization_code&redirect_uri=${redirect}`, body);
-		console.log('twitch token resp: ', raw.data);
+		//console.log('twitch token resp: ', raw.data);
     return raw;
 }
 
 async function get_user_id(token, client_id){
-    const raw_user_id = await fetch(`/cors-proxy/helix/users?`, {
+    const raw_user_id = await axios({
+        url: 'https://api.twitch.tv/cors-proxy/helix/users?', 
         method: "GET",
         headers: {
             'Authorization': `Bearer ${token}`,
@@ -27,7 +28,8 @@ async function get_user_id(token, client_id){
 }
 
 async function get_user_subscription(channel_id, user_id, token, client_id){
-    const raw_response = await fetch(`/cors-proxy/helix/subscriptions/user?broadcaster_id=${channel_id}&user_id=${user_id}`, {
+    const raw_response = await axios({
+        url: `https://api.twitch.tv/cors-proxy/helix/subscriptions/user?broadcaster_id=${channel_id}&user_id=${user_id}`,
         method: "GET",
         headers: {
           'Authorization': `Bearer ${token}`,
