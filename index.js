4a0fd51637a2b3bea2ae94331afaf330de1c7906
Kyle Zhang <kyle.z0929@gmail.com>
2021-06-13 16:39:00 -0400

    fixing encodeuri

--- index.js
+++ index.js
@@ -4,9 +4,8 @@ function getAuth(client_id, redirect_uri, scope_entities){
     
     var scope = ""
     scope_entities.forEach(element => {
-        scope.concat(" " + element)
+        scope.concat(encodeURIComponent(" ") + element)
     });
-    console.log(encodeURIComponent(scope));
     return `https://id.twitch.tv/oauth2/authorize?client_id=${client_id}&redirect_uri=${redirect_uri}&response_type=code&scope=${encodeURIComponent(scope)}`;
 }
 
